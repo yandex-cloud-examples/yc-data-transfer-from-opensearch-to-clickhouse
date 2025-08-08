a3c069a922e70c162c2c9ef9e730144bf89ad42f
gonobobelka <yuda4@yandex.ru>
2025-08-08 11:40:37 +0300

    YCDOCIO-4931: fixes 1

--- opensearch-to-clickhouse.tf
+++ opensearch-to-clickhouse.tf
@@ -166,7 +166,7 @@ resource "yandex_mdb_clickhouse_database" "mch-db" {
   name       = local.mch_db_name
 }
 
-resource "yandex_mdb_clickhouse_user" "mch-username" {
+resource "yandex_mdb_clickhouse_user" "mch-user" {
   cluster_id = yandex_mdb_clickhouse_cluster.mych.id
   name       = local.mch_username
   password   = local.mch_user_password
